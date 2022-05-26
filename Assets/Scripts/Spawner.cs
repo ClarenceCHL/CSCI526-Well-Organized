58ec73e9810c570443385460dbe51041322b034a
Kuan-Ying Wang <johnnywang1996@gmail.com>
2022-05-26 16:42:54 -0700

    More consistent box drop

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,22 +6,27 @@ public class Spawner : MonoBehaviour
 {
 
     public GameObject objectToSpawn;
+
+    private float dropRate = 2.0f; //drop a box every 2 seconds
+    private float nextDrop =0.0f; 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(objectToSpawn, new Vector3(Random.Range(0, 8), 15, 0), Quaternion.identity);
+       // Instantiate(objectToSpawn, new Vector3(Random.Range(0, 8), 15, 0), Quaternion.identity);
     }
 
-    int counter = 0;
+
+    float time =0; 
     // Update is called once per frame
     void Update()
     {
-        counter++;
-            if (counter == 1000)
-            {
-                Instantiate(objectToSpawn, new Vector3(Random.Range(0, 8), 15, 0), Quaternion.identity);
-                 counter = 0;
-            }
+        time += Time.deltaTime;
         
+        if (time > nextDrop)
+        {
+            nextDrop = time+ dropRate;
+            Instantiate(objectToSpawn, new Vector3(Random.Range(0, 8), 15, 0), Quaternion.identity);
+         
+        }  
     }
 }
