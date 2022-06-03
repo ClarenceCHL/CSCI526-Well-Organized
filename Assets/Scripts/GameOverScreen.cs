120fca045cc45dd213db6b0ec62991de5241ad74
Moxxie_YangYX <70103867+HermannYang@users.noreply.github.com>
2022-06-03 10:38:50 -0700

    Add files via upload
    Update the gameover and headcheck script

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -15,7 +15,7 @@ public class GameOverScreen : MonoBehaviour
 
     public void RestartButton()
     {
-        SceneManager.LoadScene("Start");
+        SceneManager.LoadScene("SampleScene");
     }
 
 }
