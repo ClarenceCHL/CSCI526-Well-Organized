2a039df9a4fd7520bba33508d8cf4d1f6e3f06d9
xuzihengzion <106217106+xuzihengzion@users.noreply.github.com>
2022-06-28 18:27:01 -0700

    Add files via upload

---
+++ Assets/Scripts/Kaiguan.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Kaiguan : MonoBehaviour
+{
+   public void kaiguan(){
+       gameObject.SetActive(true);
+   }
+
+    public void kaiguan2(){
+       gameObject.SetActive(false);
+   }
+}
