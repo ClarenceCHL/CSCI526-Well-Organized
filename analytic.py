2187efc9c280ef978b551144b96d331f2653d825
xgao1283 <77721942+xgao1283@users.noreply.github.com>
2022-06-28 21:32:10 -0700

    Delete analytic.py

--- analytic.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def parse():
-    f = open("data", "r")
-    jsonFile = {"result":{}}
-    for line in f:
-        data = json.loads(line)
-        jsonFile.get("result").get
-        
